8ea3be067e23485344f2ff0ea4caf4fe16946544
João Victor Oliveira <joaoliveiraa7@gmail.com>
2024-01-28 00:38:54 -0300

    feat: Add page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Hero from "@/components/main/Hero";
-
+import Skills from "@/components/main/Skills";
 export default function Home() {
 
 
@@ -9,6 +9,7 @@ export default function Home() {
     <main className="h-full w-full">
      <div className="flex flex-col w-full h-full">
         <Hero />
+        <Skills />
       </div>
     </main>
   );
