b67d259bd24c29d52a4a2cabb3930df39ceae6aa
João Victor Oliveira <joaoliveiraa7@gmail.com>
2024-01-28 00:51:49 -0300

    feat: Add scroll progress to the page

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,27 @@
 import Hero from "@/components/main/Hero";
 import Skills from "@/components/main/Skills";
 import Projects from "@/components/main/Projects";
+import { motion, useScroll, useSpring } from "framer-motion";
 export default function Home() {
+  
+  const { scrollYProgress } = useScroll();
 
+  const scaleX = useSpring(scrollYProgress);
 
   return (
     <main className="h-full w-full">
+      <motion.div
+        style={{
+          scaleX,
+          background: "linear-gradient(to right, #a855f7, #06b6d4);",
+          transformOrigin: "left",
+          position: "sticky",
+          top: 0,
+          width: "100%",
+          height: "1px",
+          zIndex: 100,
+        }}
+      />
      <div className="flex flex-col w-full h-full">
         <Hero />
         <Skills />
