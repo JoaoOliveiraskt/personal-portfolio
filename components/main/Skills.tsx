1bed8b8fd866004548e75f82d2c26db63ff78cb8
João Victor Oliveira <joaoliveiraa7@gmail.com>
2024-05-19 04:21:07 -0300

    style: Update Skills component UI

--- components/main/Skills.tsx
+++ components/main/Skills.tsx
@@ -2,25 +2,26 @@ import React from "react";
 import { motion } from "framer-motion";
 import SliderInfinite from "../sub/Slider";
 import Balancer from "react-wrap-balancer";
-
+import Divider from "@/components/ui/divider";
 
 const Skills = () => {
   return (
     <motion.section
       id="skills"
-      className="overflow-x-hidden flex space-y-10 flex-col items-center justify-center w-full h-full px-20 med:px-2 med:pt-28 pt-52"
+      className="overflow-x-hidden flex flex-col items-center justify-center w-full h-full px-20 med:px-2 med:py-32 py-60"
     >
-      <div className="flex flex-col items-center justify-center">
-        <div className="relative text-center">
-          <div className="space-y-6 tracking-wider">
-            <p className=" text-primary text-xl sm:text-base font-medium">
-              <Balancer>Criando experiências digitais com...</Balancer>
-            </p>
-          </div>
+      <div className="flex flex-col max-w-[60rem] gap-12 sm:gap-10">
+        <div className="flex flex-col gap-6">
+          <p className="tracking-wider text-center text-5xl sm:text-3xl font-semibold pb-2 bg-gradient-to-r from-primary via-primary to-text text-transparent bg-clip-text">
+            <Balancer>Criando experiências digitais com...</Balancer>
+          </p>
+          <Divider className="hidden sm:block" />
+
+          <SliderInfinite />
         </div>
+
+        <Divider className="sm:hidden" />
       </div>
-    
-      <SliderInfinite />
     </motion.section>
   );
 };
