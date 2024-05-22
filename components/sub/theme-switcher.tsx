4573814fd86b88139bcecd39a1946c052c250e0d
João Victor Oliveira <joaoliveiraa7@gmail.com>
2024-05-22 10:09:52 -0300

    feat: Update theme switcher button to use RxMoon icon

--- components/sub/theme-switcher.tsx
+++ components/sub/theme-switcher.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 import * as React from "react";
-import { Moon, Sun } from "lucide-react";
+import { Sun } from "lucide-react";
+import { RxMoon } from "react-icons/rx";
 import { useTheme } from "next-themes";
 
 import { Button } from "@/components/ui/button";
@@ -27,7 +28,7 @@ export function ModeToggle() {
           className="cursor-pointer outline-none h-8 w-8 border-none"
         >
           <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <RxMoon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
