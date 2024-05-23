aea4a03ad727248d82bc335954e5786f2da5ea88
João Victor Oliveira <joaoliveiraa7@gmail.com>
2024-05-23 11:57:52 -0300

    chore: Update npm dependencies

--- components/sub/EmailForm.tsx
+++ components/sub/EmailForm.tsx
@@ -1,6 +1,6 @@
 import React, { useRef, useState, useEffect } from "react";
 import { Button } from "../ui/button";
-import {unstable_noStore as noStore} from "next/cache"
+import { unstable_noStore as noStore } from "next/cache";
 import emailjs from "@emailjs/browser";
 import HandleOnMouseMove from "@/utils/hoverEffect";
 
@@ -27,7 +27,7 @@ export default function EmailForm() {
   const messageRef = useRef<HTMLTextAreaElement>(null);
   const [loading, setLoading] = useState(false);
   const [emailSent, setEmailSent] = useState(false);
-  
+
   const numberError = "Nome não pode conter números";
   const formError = "Preencha todos os campos.";
   const [error, setError] = useState<string>("");
@@ -40,7 +40,7 @@ export default function EmailForm() {
 
     try {
       setLoading(true);
- 
+
       if (nameRef.current && emailRef.current && messageRef.current) {
         const containsNumbers = /\d/.test(nameRef.current.value);
 
@@ -86,78 +86,87 @@ export default function EmailForm() {
 
   return (
     <div className="h-fit min-w-[19rem] relative">
-      
       <div className="p-[1px] rounded-xl dark:bg-gradient-to-bl from-zinc-100/20 to-zinc-300/5">
-      <div className="h-[23rem] relative bg-gradient-to-bl from-card to-background row-span-2 rounded-xl border border-zinc-200 dark:border-none shadow-sm dark:shadow-none"
-      onMouseMove={(e) => HandleOnMouseMove(e, setMousePosition)}>
-        <div className="flex flex-col h-full justify-between gap-5 p-4">
-          <div className="flex gap-4 items-center">
-            <div className="w-12 z-10 h-12 rounded-full dark:bg-zinc-800 flex items-center justify-center border border-zinc-200 dark:border-zinc-700">
-              <BsSend />
-            </div>
-            <div>
-              <span className="text-lg font-medium text-primary">Envie-me um e-mail</span>
-            </div>
-          </div>
-
-          <div className="flex flex-col">
-            <form onSubmit={sendEmail} className="flex flex-col gap-5 z-20">
-
-              <div className="grid grid-cols-1 gap-5">
-                <div className="w-full">
-                  <Input
-                    type="text"
-                    placeholder="Nome"
-                    name="username"
-                    autoComplete="name"
-                    className="py-5 bg-muted"
-                    ref={nameRef}
-                  />
-                </div>
-                <div className="">
-                  <Input
-                    type="email"
-                    placeholder="E-mail"
-                    name="email"
-                    autoComplete="email"
-                    className="py-5 bg-muted"
-                    ref={emailRef}
-                  />
-                </div>
+        <div
+          className="h-[23rem] relative bg-gradient-to-bl from-card to-background row-span-2 rounded-xl border border-zinc-200 dark:border-none shadow-sm dark:shadow-none"
+          onMouseMove={(e) => HandleOnMouseMove(e, setMousePosition)}
+        >
+          <div className="flex flex-col h-full justify-between gap-5 p-4">
+            <div className="flex gap-4 items-center">
+              <div className="w-12 z-10 h-12 rounded-full dark:bg-zinc-800 flex items-center justify-center border border-zinc-200 dark:border-zinc-700">
+                <BsSend />
               </div>
+              <div>
+                <span className="text-lg font-medium text-primary">
+                  Envie-me um e-mail
+                </span>
+              </div>
+            </div>
 
-              <Textarea
-                placeholder="Mensagem"
-                name="text"
-                className="bg-muted"
-                ref={messageRef}
-              ></Textarea>
+            <div className="flex flex-col">
+              <form onSubmit={sendEmail} className="flex flex-col gap-5 z-20">
+                <div className="grid grid-cols-1 gap-5">
+                  <div className="w-full">
+                    <Input
+                      type="text"
+                      placeholder="Nome"
+                      name="username"
+                      autoComplete="name"
+                      className="py-5 bg-muted"
+                      ref={nameRef}
+                    />
+                  </div>
+                  <div className="">
+                    <Input
+                      type="email"
+                      placeholder="E-mail"
+                      name="email"
+                      autoComplete="email"
+                      className="py-5 bg-muted"
+                      ref={emailRef}
+                    />
+                  </div>
+                </div>
 
-              
-              {error ? <p className="text-red-500 text-center text-sm">{error}</p> : <></>}
-              
+                <Textarea
+                  placeholder="Mensagem"
+                  name="text"
+                  className="bg-muted"
+                  ref={messageRef}
+                ></Textarea>
 
-             <div className="flex items-center justify-center">
-             {emailSent ? (
-                <Button variant={"secondary"} size={"sm"} className="bg-green-600 w-full">
-                  <MdOutlineDownloadDone />
-                </Button>
-              ) : (
-                <Button  disabled={loading} variant={"secondary"} size={"sm"} className="w-full">
-                  {loading ? (
-                    <BiLoaderAlt className="animate-spin infinite" />
+                {error ? (
+                  <p className="text-red-500 text-center text-sm">{error}</p>
+                ) : (
+                  <></>
+                )}
+
+                <div className="flex items-center justify-center">
+                  {emailSent ? (
+                    <Button size={"sm"} className="bg-green-600 w-full">
+                      <MdOutlineDownloadDone />
+                    </Button>
                   ) : (
-                    "Enviar"
+                    <Button
+                      disabled={loading}
+                      variant={"secondary"}
+                      size={"sm"}
+                      className="w-full"
+                      type="submit"
+                    >
+                      {loading ? (
+                        <BiLoaderAlt className="animate-spin infinite" />
+                      ) : (
+                        "Enviar"
+                      )}
+                    </Button>
                   )}
-                </Button>
-              )}
-             </div>
-
-            </form>
+                </div>
+              </form>
+            </div>
           </div>
+          <RadialGradient className="z-10" mousePosition={mousePosition} />
         </div>
-        <RadialGradient className="z-10" mousePosition={mousePosition}/>
-      </div>
       </div>
     </div>
   );
