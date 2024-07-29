5e85264f5d2bde1c79fced8f65ff7ee3fa83bb7b
João Oliveira <joaoliveiraa7@gmail.com>
2024-07-29 20:41:18 -0300

    refactor: Update Hero component text to improve clarity

--- components/main/Hero.tsx
+++ components/main/Hero.tsx
@@ -20,10 +20,10 @@ const Hero = () => {
             <GithubBtn />
             <h1 className="tracking-tighter font-bold leading-tight text-6xl med:text-4xl  max-w-[900px] bg-gradient-to-r from-primary via-primary to-muted-foreground text-transparent bg-clip-text">
               <Balancer>
-                Unindo design e código, sigo criando projetos{" "}
+                Unindo arte e lógica para desenvolver projetos{" "}
                 <span id="underline" className="">
                   <span className="relative inline-flex items-end justify-center bg-gradient-to-r from-primary via-primary to-muted-foreground text-transparent bg-clip-text">
-                    únicos.
+                  inovadores.
                     <Underline />
                   </span>
                 </span>
